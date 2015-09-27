fb269295ebc197cc43a31ac888c6bdf8e6cd024b
Gustavo Bazan <gustavo.bazan@koombea.com>
2015-09-26 21:08:15 -0500

    Fix som e tests

--- spec/slack/session_spec.rb
+++ spec/slack/session_spec.rb
@@ -9,17 +9,21 @@ describe Slack::Session do
     expect { Slack::Session.new(1) }.to raise_error(ArgumentError)
   end
 
-  describe '#clean_params' do
-    let(:params) { { 'test_1' => 'test', 'test_2' => nil } }
-    it 'removes nil params' do
-      expect(clean_params(params)).to eq('test_1' => 'test')
-    end
-  end
+  describe "instance methods" do
+    subject(:session) { Slack::Session.new('') }
 
-  describe '#make_query_string' do
-    let(:params) { { 'test_1' => 'test', 'test_2' => 'test' } }
-    it 'transform params to query string' do
-      expect(make_query_string(params)).to eq('test_1=test&test_2=test')
+    describe '#clean_params' do
+      let(:params) { { 'test_1' => 'test', 'test_2' => nil } }
+      it 'removes nil params' do
+        expect(session.clean_params(params)).to eq('test_1' => 'test')
+      end
+    end
+
+    describe '#make_query_string' do
+      let(:params) { { 'test_1' => 'test', 'test_2' => 'test' } }
+      it 'transform params to query string' do
+        expect(session.make_query_string(params)).to eq('test_1=test&test_2=test')
+      end
     end
   end
 end
